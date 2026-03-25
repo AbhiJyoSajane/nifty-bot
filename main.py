872ca6a7331e2c2c899c441ac45a91e302daa88f
AbhiJyoSajane <sajane.abhishek@gmail.com>
2026-03-25 14:11:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Nifty bot is running...")
+import os
+import time
+from kiteconnect import KiteConnect
+
+api_key = os.getenv("API_KEY")
+access_token = os.getenv("ACCESS_TOKEN")
+
+kite = KiteConnect(api_key=api_key)
+kite.set_access_token(access_token)
+
+while True:
+    try:
+        price = kite.ltp("NSE:INFY")["NSE:INFY"]["last_price"]
+        print("INFY price:", price)
+
+    except Exception as e:
+        print("Error:", e)
+
+    time.sleep(60)
