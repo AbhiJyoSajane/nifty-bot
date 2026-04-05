600795c312d96960bde3861dbc1866f9cc645ff5
AbhiJyoSajane <sajane.abhishek@gmail.com>
2026-04-05 19:44:16 +0530

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,7 +1,7 @@
 from kiteconnect import KiteConnect
 
 API_KEY = "btj1h6qbwop4gah2"
-ACCESS_TOKEN = "hmVK06HJzG36Da5tpDT6j6OigxDVNgfs"
+ACCESS_TOKEN = "pQ7LcW4G7PHA70VwvSvWQQW8mzQL7Ttz"
 
 kite = KiteConnect(api_key=API_KEY)
 kite.set_access_token(ACCESS_TOKEN)
