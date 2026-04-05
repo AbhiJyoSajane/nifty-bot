c96c21b1adc2924e3881cfe85774eca870b7c88e
AbhiJyoSajane <sajane.abhishek@gmail.com>
2026-04-05 22:44:52 +0530

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,10 +1,14 @@
 from kiteconnect import KiteConnect
 
-kite = KiteConnect(api_key="btj1h6qbwop4gah2")
+API_KEY = "btj1h6qbwop4gah2"
+API_SECRET = "3r0r7i5b13ll8um9r6wtwrtwv51rv4ev"
+REQUEST_TOKEN = "uWQQhOZ059xF2jO2zz5Lg5Ikp2SWm6bc"
 
-data = kite.generate_session(
-    "pQ7LcW4G7PHA70VwvSvWQQW8mzQL7Ttz",
-    api_secret="3r0r7i5b13ll8um9r6wtwrtwv51rv4ev"
-)
+kite = KiteConnect(api_key=API_KEY)
 
-print(data["access_token"])
+data = kite.generate_session(REQUEST_TOKEN, api_secret=API_SECRET)
+ACCESS_TOKEN = data["access_token"]
+
+kite.set_access_token(ACCESS_TOKEN)
+
+print("✅ Connected successfully")
