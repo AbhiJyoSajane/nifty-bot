e17cf3e3c3a5ed9fa12b17e4be718ee738d35f1f
AbhiJyoSajane <sajane.abhishek@gmail.com>
2026-04-05 21:26:48 +0530

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,13 +1,10 @@
 from kiteconnect import KiteConnect
 
-API_KEY = "btj1h6qbwop4gah2"
-ACCESS_TOKEN = "pQ7LcW4G7PHA70VwvSvWQQW8mzQL7Ttz"
+kite = KiteConnect(api_key="btj1h6qbwop4gah2")
 
-kite = KiteConnect(api_key=API_KEY)
-kite.set_access_token(ACCESS_TOKEN)
+data = kite.generate_session(
+    "pQ7LcW4G7PHA70VwvSvWQQW8mzQL7Ttz",
+    api_secret="3r0r7i5b13ll8um9r6wtwrtwv51rv4ev"
+)
 
-# Test connection
-profile = kite.profile()
-
-print("✅ Connected to Zerodha")
-print("User:", profile["user_name"])
+print(data["access_token"])
