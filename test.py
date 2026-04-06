01d6e8b7218754c57a87fd05dba073572cd49b7a
AbhiJyoSajane <sajane.abhishek@gmail.com>
2026-04-06 18:13:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from kiteconnect import KiteConnect
 api_key = os.environ.get("btj1h6qbwop4gah2")
 api_secret = os.environ.get("3r0r7i5b13ll8um9r6wtwrtwv51rv4ev")
 request_token = os.environ.get("lABed3OluETo6DOSs2ZJ4i5z77MnDzv5")
-
+print("DEBUG TOKEN:", request_token)
 kite = KiteConnect(api_key=api_key)
 
 try:
