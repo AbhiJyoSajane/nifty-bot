557f6f74a7c1d9c500689765acc1e4eee2dab666
AbhiJyoSajane <sajane.abhishek@gmail.com>
2026-04-06 19:04:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,19 @@ import os
 from kiteconnect import KiteConnect
 
 api_key = os.environ.get("API_KEY")
+api_secret = os.environ.get("API_SECRET")
+request_token = os.environ.get("REQUEST_TOKEN")
+
+print("Starting token generation...")
 
 kite = KiteConnect(api_key=api_key)
 
-print("LOGIN URL:")
-print(kite.login_url())
+try:
+    data = kite.generate_session(request_token, api_secret=api_secret)
+    
+    access_token = data["access_token"]
+    
+    print("ACCESS TOKEN:", access_token)
+
+except Exception as e:
+    print("ERROR:", e)
